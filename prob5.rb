03508be8a8e0b2a1df97d3f9f0cc435053a9fe96
Anthony Ybarra <ovfan@outlook.com>
2024-02-20 19:39:08 -0600

    Module as namespace

---
+++ prob5.rb
@@ -0,0 +1,10 @@
+module Payments
+    class Invoice
+        puts "This is an invoice"
+    end
+    class Receipt 
+        puts "This is a receipt"
+    end
+end
+Payments::Invoice
+Payments::Receipt
